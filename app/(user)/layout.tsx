4b685f2fb544ebfb18f5c2a68c97d05f1f2bb63c
Jees Vincent <jeesvincent0@gmail.com>
2026-02-15 03:35:37 +0530

    chore(auth): home page side bar initialized

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,4 +1,18 @@
-import ShInput from "@/components/atoms/ShInput";
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/atoms/breadcrumb";
+import { Separator } from "@/components/atoms/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/atoms/sidebar";
 import CreateChannelButton from "@/components/molecules/CreateChannelButton";
 import NotificationButton from "@/components/molecules/NotificationButton";
 import ProfileButton from "@/components/molecules/ProfileButton";
@@ -11,7 +25,7 @@ import React from "react";
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <div className="relative flex justify-between items-center w-full px-10 py-4 border-b">
+      <nav className="z-50 top-0  bg-background fixed flex justify-between items-center w-full px-10 py-4 border-b">
         <div className="">
           <Image
             className="hidden dark:block"
@@ -43,6 +57,43 @@ const layout = ({ children }: { children: React.ReactNode }) => {
             <ProfileButton />
           </div>
         </div>
+      </nav>
+      <div className="pt-20">
+        <SidebarProvider className={``}>
+          <AppSidebar className={'top-16 h-[calc(100vh-4rem)] bg-background'}/>
+          <SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+              <div className="flex items-center gap-2 px-4">
+                <SidebarTrigger className="-ml-1" />
+                <Separator
+                  orientation="vertical"
+                  className="mr-2 data-[orientation=vertical]:h-4"
+                />
+                <Breadcrumb>
+                  <BreadcrumbList>
+                    <BreadcrumbItem className="hidden md:block">
+                      <BreadcrumbLink href="#">
+                        Build Your Application
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator className="hidden md:block" />
+                    <BreadcrumbItem>
+                      <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                    </BreadcrumbItem>
+                  </BreadcrumbList>
+                </Breadcrumb>
+              </div>
+            </header>
+            <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+              <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="bg-muted/50 aspect-video rounded-xl" />
+                <div className="bg-muted/50 aspect-video rounded-xl" />
+                <div className="bg-muted/50 aspect-video rounded-xl" />
+              </div>
+              <div className="bg-muted/50 min-h-screen flex-1 rounded-xl md:min-h-min" />
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
       </div>
       {children}
     </div>
