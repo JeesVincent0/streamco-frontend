2ad9447321d896fba0629187dd920449636a5bce
Jees Vincent <jeesvincent0@gmail.com>
2026-02-16 13:32:45 +0530

    fix(auth): fixed signin and signup form password validtion with strong password

--- features/auth/validators/advertiser-schema.validator.ts
+++ features/auth/validators/advertiser-schema.validator.ts
@@ -6,7 +6,13 @@ export const advertiserSignupSchema = z
     lastName: z.string().min(2, "Last name required"),
     companyName: z.string().min(2, "Company name required"),
     email: z.string().email("Invalid email"),
-    password: z.string().min(8, "Password must be in 8 charecteres"),
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters")
+      .regex(/[A-Z]/, "Must contain at least one uppercase letter")
+      .regex(/[a-z]/, "Must contain at least one lowercase letter")
+      .regex(/[0-9]/, "Must contain at least one number")
+      .regex(/[^A-Za-z0-9]/, "Must contain at least one special character"),
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
