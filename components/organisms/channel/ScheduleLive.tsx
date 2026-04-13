0e1d0fd4d10b4bf835bf04d604fc354b3523427e
Jees Vincent <jeesvincent0@gmail.com>
2026-04-13 21:46:13 +0530

    refactory(live): responsive schedule live template

--- components/organisms/channel/ScheduleLive.tsx
+++ components/organisms/channel/ScheduleLive.tsx
@@ -1,15 +1,26 @@
+"use client";
+
 import ScheduleLiveForm from "@/components/molecules/channel/ScheduleLiveForm";
 import ScheduleCalendar from "@/components/molecules/channel/ScheduledSlotCalender";
+import AdminHeaderTemplate from "@/components/templates/admin/AdminHeaderTemplate";
 import DetailedPageTemplate from "@/components/templates/admin/DetailedPageTemplate";
+import { CHANNEL_ROUTES } from "@/constants/routers/channels";
+import { useParams } from "next/navigation";
 
 const ScheduleLive = () => {
+  const channelId = useParams().id as string;
   return (
-    <DetailedPageTemplate>
-      <div className="min-h-screen flex justify-center p-4 gap-3.5">
-        <ScheduleLiveForm />
-        <ScheduleCalendar />
-      </div>
-    </DetailedPageTemplate>
+    <AdminHeaderTemplate
+      text="Scheduled Lives"
+      url={CHANNEL_ROUTES.SCHEDULED_LIVE.ROOT(channelId)}
+    >
+      <DetailedPageTemplate>
+        <div className="min-h-screen flex flex-col lg:flex-row justify-center p-4 gap-3.5">
+          <ScheduleLiveForm />
+          <ScheduleCalendar />
+        </div>
+      </DetailedPageTemplate>
+    </AdminHeaderTemplate>
   );
 };
 
